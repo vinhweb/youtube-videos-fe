b1053c7caa6be1b3f24dabf3ad1cf12b0460c7b0
vinhweb <vjhnguyen@gmail.com>
2024-05-01 05:55:41 +0700

    animate background

---
+++ animated-background/main.js
@@ -0,0 +1,32 @@
+const group = document.querySelector('.group')
+for(let i = 1; i< 30; i++){
+  const containerId = `iconContainer${i}`;
+  const container = document.createElement('div')
+  
+  container.className = 'iconContainer'
+  container.id = containerId
+  
+  group.appendChild(container)
+  addIcons(containerId)
+}
+
+function addIcons(containerId){
+  const iconContainer = document.querySelector(`#${containerId}`)
+
+  // \uf001
+  const Unicode = Array(20).fill(null).map((_, index) =>
+    String.fromCodePoint(parseInt(`f${String(index).padStart(3,0)}`, 16))
+  )
+
+  for(let i = 0; i< 50; i++){
+    const icon = document.createElement('i')
+    icon.className = `icon fas`
+    icon.innerHTML = randomIcon(Unicode)
+    iconContainer.appendChild(icon)
+  }
+}
+
+function randomIcon(values){
+  const randomIndex = Math.floor(Math.random() * values.length)
+  return values[randomIndex]
+}
---
+++ magnetic-background/main.js
@@ -0,0 +1,29 @@
+const container = document.body
+const totalBoxes = 300
+const boxes = []
+
+for (let i = 1; i <= totalBoxes; i++){
+  const box = document.createElement('div')
+  box.className = 'box'
+  container.appendChild(box)
+  boxes.push(box)
+}
+
+
+document.addEventListener('mousemove', (e) => {
+  const {clientX, clientY} = e
+
+  boxes.forEach(box => {
+    const boxRect = box.getBoundingClientRect()
+    
+    const boxCenterX = boxRect.left + boxRect.width/2
+    const boxCenterY = boxRect.top + boxRect.height/2
+
+    const deltaX = clientX - boxCenterX
+    const deltaY = clientY - boxCenterY
+
+    const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI)
+    
+    box.style.transform = `rotate(${angle}deg)`
+  })
+})
