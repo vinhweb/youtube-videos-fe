2008e1707efa773d364c3473dcba9c7dfdedf57d
vinhweb <vjhnguyen@gmail.com>
2024-05-01 03:47:44 +0700

    Initial commit

---
+++ magnetic-link/main.js
@@ -0,0 +1,26 @@
+let cursor = document.getElementById('cursor')
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.pageX}px`
+  cursor.style.top = `${e.pageY}px`
+})
+
+
+let item = document.querySelectorAll('ul li').forEach(item => {
+  item.addEventListener('mousemove', (e) => {
+    let x = e.offsetX
+    let y = e.offsetY
+
+    let itemWidth = item.clientWidth
+    let itemHeight = item.clientHeight
+
+    let transX = (x - itemWidth/2)
+    let transY = (y - itemHeight/2)
+
+    item.style.transform = `translateX(${transX}px) translateY(${transY}px)`
+  })
+
+  item.addEventListener('mouseout', (e) => {
+    item.style.transform = ``
+  })
+})
+
