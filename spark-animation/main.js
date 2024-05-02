1fb23e5270f185f9e1fdc9b962b2f24dc09cc8d6
vinhweb <vjhnguyen@gmail.com>
2024-05-02 09:20:30 +0700

    spark update

--- spark-animation/main.js
+++ spark-animation/main.js
@@ -1,28 +1,32 @@
 let body = document.querySelector('body')
 let h2 = document.querySelector('h2')
-body.addEventListener('click', (e) => {
+
+// todo: random position => around the center
+// todo: follow the mouse => oke
+
+function sparkAnimation(positionX, positionY){
   for(let i = 0; i < 50; i++){
-    let spark = document.createElement('i')
-    spark.style.top = (e.clientY - body.offsetTop) + 'px'
-    spark.style.left = (e.clientX - body.offsetLeft) + 'px'
+    const spark = document.createElement('i')
 
-    let randomX = (Math.random() - 0.5) * window.innerWidth / 1.5
-    let randomY = (Math.random() - 0.5) * window.innerHeight / 1.5
+    spark.style.left = `${positionX}px`
+    spark.style.top = `${positionY}px`
 
-    spark.style.setProperty('--randomX', randomX + 'px')
-    spark.style.setProperty('--randomY', randomY + 'px')
+    const randomX = (Math.random() - 0.5) * window.innerWidth / 1.5
+    const randomY = (Math.random() - 0.5) * window.innerWidth / 1.5
 
-    let randomSize = Math.random()*30 + 2
-    spark.style.width = randomSize+'px'
-    spark.style.height = randomSize+'px'
-    
-    let duration = Math.random() * 2 + 0.5
+    spark.style.setProperty('--randomX', `${randomX}px`)
+    spark.style.setProperty('--randomY', `${randomY}px`)
+
+    const randomSize = Math.random()*30 + 2
+    spark.style.width = `${randomSize}px`
+    spark.style.height = `${randomSize}px`
+
+    const duration = Math.random() * 2 + 0.5
     spark.style.animation = `animate ${duration}s ease-out forwards`
 
-    let colors = ['#f4034c', '#fff', '#ffeb3b', '#03a9f4']
-    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    const colors = ['#FFF67E', '#BFEA7C', '#9BCF53', '#416D19']
+    const randomColor = colors[Math.floor(Math.random() * colors.length)]
 
-    h2.style.color = randomColor
     spark.style.background = randomColor
 
     body.appendChild(spark)
@@ -31,4 +35,16 @@ body.addEventListener('click', (e) => {
       spark.remove()
     }, 2000)
   }
+}
+body.addEventListener('click', (e) => {
+  // Array(5).fill(null).forEach((_,index) => {
+  //   const randomX = (Math.random() * .5 + .25) * window.innerWidth
+  //   const randomY = (Math.random() * .6 + .15) * window.innerHeight
+  //   sparkAnimation(randomX, randomY)
+  // })
+  sparkAnimation(e.clientX, e.clientY)
 })
+
+// range Math.random()
+// max -> * +
+// min -> +
